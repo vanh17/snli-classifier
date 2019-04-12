e9f02c82a5dead7c05dad4de3b9ec37ce2e881cf
Van Nguyen Hung Hoang <vanh17@users.noreply.github.com>
2019-04-13 00:23:25 +0700

    create DataToFeature class in bayes classifier

---
+++ bayes.py
@@ -0,0 +1,9 @@
+from data_processing import data_bayes
+import sklearn
+import math
+
+class DataToFeature:
+	def __init__(self, data: Iterable[Sequene[Text], Sequene[Text], Text]):
+		""" create class object to hold important information
+		word_count
+		"""
