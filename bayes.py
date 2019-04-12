d0d6a387c03ae66643e3a4a1de55a54766a03cc3
Van Nguyen Hung Hoang <vanh17@users.noreply.github.com>
2019-04-13 05:18:09 +0700

    create prior probability

--- bayes.py
+++ bayes.py
@@ -4,22 +4,35 @@ import sklearn
 import math
 
 class bayes:
-    def __init__(self, datas: Iterable[Sequene[Text], Sequene[Text], Text]):
+    def __init__(self):
         """ create class object to hold important information
         vocaublary as a counter to hold vocabulary for each class that need to classify
         also create the count sume to keep track of the total words in each class
         """
+        self.classCount = Counter()
         self.vocabulary = Counter()
         self.vocabulary["entailment"] = Counter()
         self.vocabulary["neutral"] = Counter()
         self.vocabulary["contradiction"] = Counter()
-        self.sumE = 0
-        self.sumN = 0
-        self.sumC = 0
-        for data in datas:
+        self.priorE = 0.0
+        self.priorN = 0.0
+        self.priorC = 0.0
+
+    def train(self, datas: Iterable[Sequene[Text], Sequene[Text], Text]):
+    	N = 0
+    	for data in datas:
+    		N = N + 1
             premise, hypothesis, label = data
+            self.classCount[label] = self.classCount.get(label, 0) + 1
+            # count unigram, bigram
             for word in premise+hypothesis:
                 self.vocabulary[label][word] = self.vocabulary[label].get(word, 0) + 1
+        self.priorE = math.log(self.classCount["entailment"] / N)
+        self.priorN = math.log(self.classCount["neutral"] / N)
+        self.priorC = math.log(self.classCount["contradiction"] / N)        
+
+
+
 
 
 
