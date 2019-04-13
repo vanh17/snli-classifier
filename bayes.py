55546e060bacd82dbfcddd25d1f8684aa450fe5e
Van Nguyen Hung Hoang <vanh17@users.noreply.github.com>
2019-04-13 07:12:39 +0700

    indented correctly

--- bayes.py
+++ bayes.py
@@ -44,12 +44,12 @@ class Bayes:
     	for text in texts:
         	premise, hypothesis = text
         	for l in ["entailment", "neutral", "contradiction"]:
-            	probDict[l] = self.priors[l]
-            	for word in premise+hypothesis:
-            		if word in self.globVoc:
-            			probDict[l] += self.condprob[word][l]
-        	preds.append(max(probDict))
-    	return preds	
+				probDict[l] = self.priors[l]
+				for word in premise+hypothesis:
+					if word in self.globVoc:
+						probDict[l] += self.condprob[word][l]
+			preds.append(max(probDict))
+		return preds	
                         
 
 
