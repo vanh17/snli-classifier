a5306864a82568929bd60b793b2fa2791b035526
Van Nguyen Hung Hoang <vanh17@users.noreply.github.com>
2019-04-25 09:56:41 +0700

    Create data_parser for RNN class
    This will help to extract the needed information based on the need of RNN

--- data_processing.py
+++ data_processing.py
@@ -50,3 +50,33 @@ class Data_bayes:
                 if len(word) > 0:
                     posList.append((word[0].lower(), currPos))
         return posList
+
+class Data_RNN:
+    def read_to_turple(self, filename: Text, is_lemmatized=False, is_stemmed=False) -> Iterable[Tuple[Text, Text, Text]]:
+        """Generate (premise, hypothesis, label) turple for each sentence in the training, dev, test data.
+            the structure for each line in the file name is followed:
+            gold_label  premise_binary_parse    hypothesis_binary_parse premise_parse   hypothesis_parse
+            The first line of the file is the tittle for each item, so we will ignore this line
+            Also ignore the lines where the first item in the line is _, because that means all the annotators 
+            could not agree on the label for such pairs.
+        """
+        with io.open(filename, "r") as data:
+            lemmatizer = WordNetLemmatizer()
+            stemmer = PorterStemmer()
+            for line in data:
+                line = line.split('\t')
+                if line[0] == "gold_label" or line[0] == "-":
+                    continue
+                label = line[0]
+                if not is_stemmed and not is_lemmatized:
+                    premise = [c for c in nltk.word_tokenize(line[5])]
+                    hypothesis = [c for c in nltk.word_tokenize(line[6])]
+                if is_stemmed:
+                    premise = [stemmer.stem(c) for c in nltk.word_tokenize(line[5])]
+                    hypothesis = [stemmer.stem(c) for c in nltk.word_tokenize(line[6])]
+                if is_lemmatized:
+                    premise = [lemmatizer.lemmatize(c) for c in nltk.word_tokenize(line[5])]
+                    hypothesis = [lemmatizer.lemmatize(c) for c in nltk.word_tokenize(line[6])]
+                prePos = self.extract_pos(line[3])
+                hypoPos = self.extract_pos(line[4])
+                yield ((" ".join(premise), " ".join(hypothesis), label))
